eaa8e9e3803b80149e94ba206521c596b74bf1b3
198176 <adriantomczak36@gmail.com>
2018-01-18 19:44:42 +0100

    [isolation] Test Case 2 - when

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 
@@ -39,12 +40,19 @@ public class BookKeeperTest {
     }
 
     @Test
-    public void testIssuanceInvoiceWithOneItem() {
+    public void testIssuanceInvoiceWithOneItemReturnInvoiceWithOneItem() {
         int sizeItems = 1;
         when(invoiceRequest.getItems()).thenReturn(Collections.singletonList(requestItem));
         when(taxPolicy.calculateTax(ProductType.FOOD, requestItem.getTotalCost()))
                 .thenReturn(new Tax(new Money(0.25), "Item Tax"));
         invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
         assertThat(invoice.getItems().size(), is(sizeItems));
+    }
+
+    @Test
+    public void testIssuanceInvoiceWithTwoItemCallTwiceCalculateTax() {
+        when(invoiceRequest.getItems()).thenReturn(Arrays.asList(requestItem, requestItem));
+        when(taxPolicy.calculateTax(ProductType.FOOD, requestItem.getTotalCost()))
+                .thenReturn(new Tax(new Money(0.25), "Item Tax"));
     }
 }
