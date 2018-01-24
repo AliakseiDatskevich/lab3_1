f4722eb290c0ef564e29b86eef020681d4af0b0e
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2018-01-24 17:24:19 +0100

    isolation - Dodanie metody setUp()

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Date;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -17,23 +18,35 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeperTest {
 
+	Money money;
+	InvoiceFactory invoiceFactory;
+	BookKeeper bookKeeper;
+	ProductData productData;
+	RequestItem requestItem;
+	InvoiceRequest mockedInvoiceRequest;
+	TaxPolicy mockedTaxPolicy;
+	Tax tax;
+
+	@Before
+	public void setUp() {
+		money = new Money(10);
+		invoiceFactory = new InvoiceFactory();
+		bookKeeper = new BookKeeper(invoiceFactory);
+		productData = new ProductData(new Id("1"), money, "Product", ProductType.STANDARD, new Date());
+		requestItem = new RequestItem(productData, 1, money);
+		tax = new Tax(new Money(2.3), "Tax");
+		mockedInvoiceRequest = mock(InvoiceRequest.class);
+		mockedTaxPolicy = mock(TaxPolicy.class);
+
+	}
+
 	@Test
 	public void requestingAnInvoiceWithOneItemIsCorrect() {
-		Money money = new Money(10);
-		InvoiceFactory invoiceFactory = new InvoiceFactory();
-		BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
-		ProductData productData = new ProductData(new Id("1"), money, "Product", ProductType.STANDARD, new Date());
-		RequestItem requestItem = new RequestItem(productData, 1, money);
-		InvoiceRequest mockedInvoiceRequest = mock(InvoiceRequest.class);
-		TaxPolicy mockedTaxPolicy = mock(TaxPolicy.class);
-
 		when(mockedInvoiceRequest.getItems()).thenReturn(Arrays.asList(requestItem));
-		when(mockedTaxPolicy.calculateTax(ProductType.STANDARD, requestItem.getTotalCost()))
-				.thenReturn(new Tax(new Money(2.3), "Tax"));
+		when(mockedTaxPolicy.calculateTax(ProductType.STANDARD, requestItem.getTotalCost())).thenReturn(tax);
 
 		Invoice result = bookKeeper.issuance(mockedInvoiceRequest, mockedTaxPolicy);
 
 		assertThat(result.getItems().size(), Matchers.is(1));
 	}
-
 }
