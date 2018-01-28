f656685bf9ebcc2f2f8759e570a1a09dea9ae9cc
smoczynskaj <smoczynska.justyna@gmail.com>
2018-01-28 16:39:11 +0100

    isolation_Test 1: Faktura z jedną pozycją

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,55 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Justyna on 28.01.2018.
+ */
+public class BookKeeperTest {
+
+    @Test
+    public void hasOneInvoiceLine() throws Exception {
+        RequestItem requestItem = createRequestItem1();
+
+        ClientData clientData = new ClientData(Id.generate(), "Jan Kowalski");
+        InvoiceRequest invoiceRequest = new InvoiceRequest(clientData);
+        invoiceRequest.add(requestItem);
+
+        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
+        Tax tax = new Tax(new Money(0.22, Money.DEFAULT_CURRENCY), "VAT");
+        when(taxPolicy.calculateTax(Mockito.<ProductType>anyObject(), Mockito.<Money>anyObject())).thenReturn(tax);
+
+        InvoiceFactory invoiceFactory = new InvoiceFactory();
+
+        BookKeeper bookKeeper = new BookKeeper(invoiceFactory);
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        List<InvoiceLine> lines = invoice.getItems();
+        assertEquals(lines.size(), 1);
+    }
+
+    private RequestItem createRequestItem1() {
+        Money price = new Money(213.67, Money.DEFAULT_CURRENCY);
+        ProductData productData = new ProductData(Id.generate(), price, "ticket", ProductType.STANDARD, new Date());
+        return new RequestItem(productData, 2, price.multiplyBy(2));
+    }
+
+    private RequestItem createRequestItem2() {
+        Money price = new Money(3.20, Money.DEFAULT_CURRENCY);
+        ProductData productData = new ProductData(Id.generate(), price, "butter", ProductType.FOOD, new Date());
+        return new RequestItem(productData, 3, price.multiplyBy(3));
+    }
+}
