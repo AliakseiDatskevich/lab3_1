bba7d38b461b18b3dabe112ec5f2d57bd87393a0
smoczynskaj <smoczynska.justyna@gmail.com>
2018-01-29 19:03:41 +0100

    Test sprawdzający czy metoda suggestEquivalent zostanie wywołana dla niedostępnego produktu

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -13,6 +13,7 @@ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductRepository;
 import pl.com.bottega.ecommerce.sales.domain.reservation.Reservation;
 import pl.com.bottega.ecommerce.sales.domain.reservation.ReservationRepository;
 import pl.com.bottega.ecommerce.system.application.SystemContext;
+import pl.com.bottega.ecommerce.system.application.SystemUser;
 
 import static org.mockito.Mockito.*;
 
@@ -49,6 +50,18 @@ public class AddProductCommandHandlerTest {
         verify(handler.getReservationRepository(), times(1)).save(Mockito.<Reservation>any());
     }
 
+    @Test
+    public void isSuggestEquivalentCalledForUnavailableProduct() {
+        stubMethods2();
+
+        Id orderId = Id.generate();
+        Id productId = Id.generate();
+
+        handler.handle(new AddProductCommand(orderId, productId, 5));
+
+        verify(handler.getSuggestionService(), times(1)).suggestEquivalent(Mockito.<Product>any(), Mockito.<Client>any());
+    }
+
     private void stubMethods() {
         Reservation reservation = new Reservation(null, Reservation.ReservationStatus.OPENED, null, null);
         when(handler.getReservationRepository().load(Mockito.<Id>any())).thenReturn(reservation);
@@ -57,5 +70,20 @@ public class AddProductCommandHandlerTest {
         when(handler.getProductRepository().load(Mockito.<Id>any())).thenReturn(product);
 
         when(handler.getSuggestionService().suggestEquivalent(product, null)).thenReturn(product);
+    }
+
+    private void stubMethods2() {
+        Reservation reservation = new Reservation(null, Reservation.ReservationStatus.OPENED, null, null);
+        when(handler.getReservationRepository().load(Mockito.<Id>any())).thenReturn(reservation);
+
+        Product product = new Product(null, null, null, null);
+        product.markAsRemoved();
+        when(handler.getProductRepository().load(Mockito.<Id>any())).thenReturn(product);
+
+        Product suggestedProduct = new Product(null, null, null, null);
+        when(handler.getSuggestionService().suggestEquivalent(product, null)).thenReturn(suggestedProduct);
+
+        SystemUser systemUser = new SystemUser(Id.generate());
+        when(handler.getSystemContext().getSystemUser()).thenReturn(systemUser);
     }
 }
