93e62279f7b714194633892b44b00d1c5ed34bad
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2018-02-14 02:16:40 +0100

    isolation : Test Case 3 check quantity of first position on invoice

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,5 +1,6 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
@@ -17,21 +18,37 @@ import static org.mockito.Matchers.any;
 
 public class BookKeeperTest {
 
+    private Money money;
+    private Id id;
+    private String productName;
+    private String description;
+    private BookKeeper bookKeeper;
+    private InvoiceRequest invoiceRequest;
+    private ProductData productData;
+    private TaxPolicy taxPolicy;
+    private RequestItem requestItem;
+    private int quantity;
+
+    @Before
+    public void initialize() {
+        money = new Money(12, "USD");
+        id = new Id("12");
+        productName = "Onion";
+        description = "VEGE";
+        quantity = 2;
+
+        bookKeeper = new BookKeeper(new InvoiceFactory());
+        invoiceRequest = Mockito.mock(InvoiceRequest.class);
+        productData = new ProductData(id, money, productName, ProductType.FOOD, new Date());
+        requestItem = new RequestItem(productData, quantity, money);
+        taxPolicy = Mockito.mock(TaxPolicy.class);
+    }
+
     @Test
     public void issuanceRequestForInvoiceWithOnePosition() {
-        Money money = new Money(12, "USD");
-        Id id = new Id("12");
-        String productName = "Onion";
-        String description = "VEGE";
         int expectedSize = 1;
-
-        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-        InvoiceRequest invoiceRequest = Mockito.mock(InvoiceRequest.class);
-        ProductData productData = new ProductData(id, money, productName, ProductType.FOOD, new Date());
         List<RequestItem> requestItems = new ArrayList<>();
-        RequestItem requestItem = new RequestItem(productData, 2, money);
         requestItems.add(requestItem);
-        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
 
         Mockito.when(invoiceRequest.getItems()).thenReturn(requestItems);
         Mockito.when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class)))
@@ -45,19 +62,9 @@ public class BookKeeperTest {
 
     @Test
     public void issuanceRequestForInvoiceWithTwoPositionCallCalculateTaxTwice() {
-        Money money = new Money(12, "USD");
-        Id id = new Id("12");
-        String productName = "Onion";
-        String description = "VEGE";
-
-        BookKeeper bookKeeper = new BookKeeper(new InvoiceFactory());
-        InvoiceRequest invoiceRequest = Mockito.mock(InvoiceRequest.class);
-        ProductData productData = new ProductData(id, money, productName, ProductType.FOOD, new Date());
         List<RequestItem> requestItems = new ArrayList<>();
-        RequestItem requestItem = new RequestItem(productData, 2, money);
         requestItems.add(requestItem);
         requestItems.add(requestItem);
-        TaxPolicy taxPolicy = Mockito.mock(TaxPolicy.class);
 
         Mockito.when(invoiceRequest.getItems()).thenReturn(requestItems);
         Mockito.when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class)))
@@ -65,7 +72,22 @@ public class BookKeeperTest {
 
         bookKeeper.issuance(invoiceRequest, taxPolicy);
 
-        Mockito.verify(taxPolicy, Mockito.times(2)).calculateTax(any(ProductType.class), any(Money.class));
+        Mockito.verify(taxPolicy, Mockito.times(2))
+                .calculateTax(any(ProductType.class), any(Money.class));
+    }
+
+    @Test
+    public void issuanceRequestForInvoiceCheckQuantityOfFirstPosition() {
+        List<RequestItem> requestItems = new ArrayList<>();
+        requestItems.add(requestItem);
+
+        Mockito.when(invoiceRequest.getItems()).thenReturn(requestItems);
+        Mockito.when(taxPolicy.calculateTax(any(ProductType.class), any(Money.class)))
+                .thenReturn(new Tax(money, description));
+
+        Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+
+        assertThat(invoice.getItems().get(0).getQuantity(), is(quantity));
     }
 
 }
